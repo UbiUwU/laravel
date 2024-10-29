c0a15a8d86068fbfd4dbc067c31e9132967c8d2e
UbiUwU <keflok23@gmail.com>
2024-10-29 17:39:43 -0500

    Prueba de lo que estoy haciedo

---
+++ example-app/resources/views/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+...
+<link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
+<title>@yield('title', 'Online Store')</title>
+</head>
+<body>
+…
+<!-- footer -->
+<div class="copyright py-4 text-center text-white">
+<div class="container">
+<small>
+Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank"
+href="https://twitter.com/danielgarax">
+Daniel Correa
+</a> - <b>Paola Vallejo</b>
+</small>
+</div>
+</div>
+<!-- footer -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+</script>
+</body>
+</html>
